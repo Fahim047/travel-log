90f47affc400e8ddfdaac3d5fc96d2fd59898d6e
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2025-06-06 21:47:33 +0600

    ✨ add light-dark theme toggle (#51)

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="navbar bg-base-200 text-primary-content">
+  <div class="navbar bg-base-100 text-primary-content">
     <div class="navbar-start">
-      <NuxtLink to="/" class="btn btn-ghost text-xl">
+      <NuxtLink to="/" class="font-bold text-emerald-600 text-xl">
         Travel Log
       </NuxtLink>
     </div>
-    <div class="navbar-end">
+    <div class="navbar-end gap-2">
+      <AppThemeToggle />
       <button class="btn btn-accent">
         Sign In With Github
         <Icon name="tabler:brand-github" size="24" />
