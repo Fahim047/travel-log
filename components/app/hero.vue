4a97715a6c1abb63747680e5905ce345e74b91a2
Fahimul Islam <fahimulislam58@gmail.com>
2025-06-06 16:16:09 +0600

    ✨ add landing page

---
+++ components/app/hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-100 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="py-6">
+          Keep track of your travel and adventures. Add locations, photos and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
