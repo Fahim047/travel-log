6ace82001086679080600bc1904d0f8abb78e908
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2025-06-09 22:59:16 +0600

    ✨ setup database (#53)

---
+++ lib/db/schema/location-log.ts
@@ -0,0 +1,17 @@
+import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { location } from "./location";
+
+export const locationLog = sqliteTable("locationLog", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  slug: text().notNull(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  startedAt: int().notNull(),
+  endedAt: int().notNull(),
+  locationId: int().notNull().references(() => location.id),
+  description: text(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
