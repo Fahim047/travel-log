93a2f25c19f468d3f0f6ac34c1ef0ff951fda7c6
Fahimul Islam <57781871+Fahim047@users.noreply.github.com>
2025-06-21 09:59:42 +0600

    display profile and add sign out (#56)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -3,9 +3,36 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button class="btn btn-accent" @click="authStore.signIn">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="size-6 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/dashboard">
+          Dashboard
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sign In With Github
     <span v-if="authStore.loading" class="loading loading-spinner loading-xs" />
-    <Icon v-else name="tabler:brand-github" size="24" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
   </button>
 </template>
