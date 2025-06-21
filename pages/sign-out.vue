d7684e663f56dbe5bda52442ca1fad355ddc77c5
Fahimul Islam <fahimulislam58@gmail.com>
2025-06-21 09:45:45 +0600

    display profile and add sign out

---
+++ pages/sign-out.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+onMounted(() => authStore.signOut());
+</script>
+
+<template>
+  <div class="card bg-base-300 max-w-3xl mx-auto min-h-75 flex justify-center items-center gap-5">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
